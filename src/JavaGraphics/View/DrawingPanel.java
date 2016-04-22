3f96db5ed9b9cb698ab8128527a7a0f375cb3f06
tristangaebler <Tristan.Gaebler@gmail.com>
2016-04-22 08:48:10 -0600

    Added the contents of the ShapePanel.

--- src/JavaGraphics/View/DrawingPanel.java
+++ src/JavaGraphics/View/DrawingPanel.java
@@ -1,14 +1,24 @@
 package JavaGraphics.View;
 
 import JavaGraphics.Controller.GraphicsController;
+import javax.swing.*;
+import java.awt.*;
 
-public class DrawingPanel
+public class DrawingPanel extends JPanel
 {
 	private GraphicsController baseController;
+	private SpringLayout baseLayout;
 	
 	public DrawingPanel(GraphicsController baseController)
 	{
 		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		
+		//====Calling the methods====
+		setUpPane();
+		setUpPanel();
+		setUpLayout();
+		setUpListeners();
 	}
 	
 	private void setUpPane()
@@ -16,11 +26,17 @@ public class DrawingPanel
 		
 	}
 	
+	/*
+	 * GUI Components
+	 */
 	private void setUpPanel()
 	{
 		
 	}
 	
+	/*
+	 * Garbage Code
+	 */
 	private void setUpLayout()
 	{
 		
--- src/JavaGraphics/View/ShapePanel.java
+++ src/JavaGraphics/View/ShapePanel.java
@@ -1,6 +1,50 @@
 package JavaGraphics.View;
 
-public class ShapePanel
-{
+import javax.swing.*;
 
+import JavaGraphics.Controller.GraphicsController;
+
+public class ShapePanel extends JPanel
+{
+	private GraphicsController baseController;
+	private SpringLayout baseLayout;
+	
+	public ShapePanel(GraphicsController baseController)
+	{
+		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		
+		//====Calling the methods====
+		setUpPane();
+		setUpPanel();
+		setUpLayout();
+		setUpListeners();
+	}
+	
+	private void setUpPane()
+	{
+		
+	}
+	
+	/*
+	 * GUI Components
+	 */
+	private void setUpPanel()
+	{
+		
+	}
+	
+	/*
+	 * Garbage Code
+	 */
+	private void setUpLayout()
+	{
+		
+	}
+	
+	private void setUpListeners()
+	{
+		
+	}
+	
 }
