4130bae85e0e7a560817016d4354ef572125ad20
tristangaebler <Tristan.Gaebler@gmail.com>
2016-04-26 08:03:14 -0600

    Added the triangle

--- src/JavaGraphics/View/ShapePanel.java
+++ src/JavaGraphics/View/ShapePanel.java
@@ -1,5 +1,10 @@
 package JavaGraphics.View;
 
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 import JavaGraphics.Controller.GraphicsController;
@@ -9,16 +14,61 @@ public class ShapePanel extends JPanel
 	private GraphicsController baseController;
 	private SpringLayout baseLayout;
 	
+	private ArrayList<Rectangle> rectangleList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Ellipse2D> circleList;
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	
+	
 	public ShapePanel(GraphicsController baseController)
 	{
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
+		
+		rectangleList = new ArrayList<Rectangle>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		circleList = new ArrayList<Ellipse2D>();
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		
 		
 		//====Calling the methods====
 		setUpPane();
 		setUpPanel();
 		setUpLayout();
 		setUpListeners();
+	}
+	
+	public void addCircle()
+	{
+		
+	}
+	
+	public void addRectangle()
+	{
+		
+	}
+	
+	public void addTriangle()
+	{
+		int [] xpoints = new int [3];
+		int [] ypoints = new int [3];
+		xpoints = new int []{(int)(Math.random() * 250), (int)(Math.random() * 250), (int)(Math.random() * 250)};
+		ypoints = new int []{(int)(Math.random() * 250), (int)(Math.random() * 250), (int)(Math.random() * 250)};
+		
+		Polygon triangle = new Polygon(xpoints, ypoints, 3);
+		triangleList.add(triangle);
+	}
+	
+	public void addSquare()
+	{
+		
+	}
+	
+	public void addElipse()
+	{
+		
 	}
 	
 	private void setUpPane()
