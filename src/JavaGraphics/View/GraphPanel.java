a55363b984427414ddb7336ff303bb4ca51eb271
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-04 07:55:36 -0600

    Added the grapghPanel

---
+++ src/JavaGraphics/View/GraphPanel.java
@@ -0,0 +1,47 @@
+package JavaGraphics.View;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class GraphPanel extends JPanel
+{
+	private int []graphArray;
+
+	
+	public GraphPanel()
+	{
+		graphArray = new int [] {1, 2, 5, 6, 8, 9,};
+		setupPanel();
+		
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.BLUE);
+	}
+	
+
+	
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		super.paintComponent(currentGraphics);
+		Graphics2D mainGraphics = (Graphics2D)currentGraphics;
+		
+		for(int index = 0; index < graphArray.length; index++)
+		{
+			int height = this.getHeight()/graphArray.length;
+			int width = (int)((graphArray[index] / 200.00) * this.getWidth());
+			int xPosition = 0;
+			int yPosition = height * index;
+			
+			int red = (int)(Math.random() * 256);
+			int blue = (int)(Math.random() * 256);
+			int green = (int)(Math.random() * 256);
+			int alpha = (int)(Math.random() * 256);
+			
+			mainGraphics.setColor(new Color(red, green, blue, alpha));
+			mainGraphics.fill(new Rectangle(xPosition, yPosition, width, height));
+		}
+	}
+}
