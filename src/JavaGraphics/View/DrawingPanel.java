df9e63c1e58a9c01abc865726329cb0c0a1facec
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-04 08:26:11 -0600

    End project commit

--- src/JavaGraphics/View/DrawingPanel.java
+++ src/JavaGraphics/View/DrawingPanel.java
@@ -1,9 +1,7 @@
 package JavaGraphics.View;
 
 import JavaGraphics.Controller.GraphicsController;
-
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
